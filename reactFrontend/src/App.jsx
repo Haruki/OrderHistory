65686624e38937bacd30a28ae1daab43b32ca488
Homer Simpson <hb@hb.gl>
2023-01-08 20:46:57 +0100

    react dev test

--- reactFrontend/src/App.jsx
+++ reactFrontend/src/App.jsx
@@ -1,19 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
-function App() {
-   
-  const title = "wurstbrot"
-
+/*Root Component */
+const App = () => {
+  const title = 'wurstbrot';
+  const arr = [
+    { id: '1', value: 'butter' },
+    { id: '2', value: 'keks' },
+    { id: '3', value: 'F1' },
+  ];
   return (
-      <div>
+    <div>
       <h1>Vite + {title}</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <List list={arr} />
+      <Search />
     </div>
-  )
-}
+  );
+};
 
-export default App
+/* List Component */
+const List = (props) => {
+  return (
+    <ol>
+      {props.list.map((x) => (
+        <Item key={x.id} item={x} />
+      ))}
+    </ol>
+  );
+};
+
+/*Item Component*/
+const Item = (props) => <li key={props.item.id}>{props.item.value}</li>;
+
+/* Search Component */
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input type='text' id='search' onChange={handleChange} />
+    </div>
+  );
+};
+
+export default App;
