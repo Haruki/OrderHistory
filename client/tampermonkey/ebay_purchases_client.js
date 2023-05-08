72462459874d8fc988dce21f2ae219f90c4445e2
Homer Simpson <hb@hb.gl>
2023-05-08 22:13:51 +0200

    ebay tampermonkey client: updated with new loop structure

--- client/tampermonkey/ebay_purchases_client.js
+++ client/tampermonkey/ebay_purchases_client.js
@@ -29,79 +29,82 @@ async function postData(url = '', data = {}) {
 
 (function () {
   'use strict';
-  let orderList = document.querySelectorAll('.m-order-card');
+  let orderList = document.querySelectorAll('.m-ph-card.m-order-card');
   console.log('Items auf Seite: %s', orderList.length);
   for (var order of orderList) {
     console.log('working on order...');
-    //artikelnummer:
-    // let artikelnummer = order.querySelector('[data-listing-id]');
-    // console.log(artikelnummer.getAttribute('data-listing-id'));
-    //itemName:
-    let itemNameElement = order.querySelector(
-      'div.m-container-item-layout-row.m-container-item-layout-row__body > div.m-container-item-layout-row__wrapper > div.container-item-col.container-item-col-item-info > div.container-item-col__info-item-info-primary.container-item-col__info-item-info-title > div > a'
-    );
-    console.log('itemName: %o', itemNameElement.text);
-    //price
-    let itemPrice = order.querySelector(
-      'div.m-container-item-layout-row.m-container-item-layout-row__body > div.m-container-item-layout-row__wrapper > div.container-item-col.container-item-col-item-info > div.container-item-col__info-item-info-primary.container-item-col__info-item-info-additionalPrice > div > span'
-    );
-    let priceNumber = /^[^\d]*(\d.+)/.exec(itemPrice.innerHTML);
-    let price;
-    if (priceNumber[1]) {
-      console.log('regex price raw: ' + typeof priceNumber[1]);
-      console.log('regex price number: ' + priceNumber[1].replace(',', ''));
-      price = priceNumber[1].replace(',', '');
-    } else {
-      console.log(
-        'non regex price: ' + itemPrice.innerHTML.substring(4).replace(',', '')
-      );
-      price = itemPrice.innerHTML.substring(4).replace(',', '');
-    }
-    //currency
-    let itemCurrency = /^[^(\s|\d)]*/.exec(itemPrice.innerHTML);
-    console.log('currency: %d', itemCurrency[0]);
-    let currency;
-    if (itemCurrency[0]) {
-      currency = itemCurrency[0].replace('EUR', '€').replace('US', '$');
-    }
-    //vendor
-    let vendorElement = order.querySelector(
-      'div.m-container-item-layout-row.m-container-item-layout-row__body > div.m-container-item-layout-row__wrapper > div.container-item-col.container-item-col-item-info > div.container-item-col__info-item-info-primary.container-item-col__info-item-info-sellerInfo > div:nth-child(2) > a > span'
-    );
-    console.log('Haendler: %s', vendorElement.firstChild.textContent);
-    //imgUrl
-    let imgElement = order.querySelector('div.m-image img[src][alt]');
-    console.log('SRC: ' + imgElement.getAttribute('src'));
-    let imgUrl = imgElement.getAttribute('src');
-    if (imgElement.getAttribute('data-imgurl')) {
-      console.log('DATA-IMGURL: ' + imgElement.getAttribute('data-imgurl'));
-      imgUrl = imgElement.getAttribute('data-imgurl');
-    }
     //orderDate
     let dateElement = order.querySelector(
       'div.primary__item.secondaryMessage span:nth-child(2).primary__item--item-text'
     );
     console.log('orderDate: %s', dateElement.textContent);
-    //build object for later json marshal
-    var orderObj = {
-      //   artikelnummer: parseint(artikelnummer.getattribute('data-listing-id')),
-      itemName: itemNameElement.text,
-      price: parseInt(price),
-      vendor: vendorElement.firstChild.textContent,
-      imgUrl: imgUrl,
-      purchaseDate: dateElement.textContent
-        .replace('Mai', 'May')
-        .replace('Okt', 'Oct')
-        .replace('Dez', 'Dec')
-        .replace('Mär', 'Mar'),
-      currency: currency,
-    };
-    console.log(JSON.stringify(orderObj));
-    //request to api
+    //loop item cards
+    let itemCards = order.querySelectorAll(
+      '.m-item-card.m-container-item-layout-row__body'
+    );
+    console.log('Items in order: %s', itemCards.length);
+    for (var itemCard of itemCards) {
+      //itemName:
+      let itemNameElement = order.querySelector('div a.nav-link');
+      console.log('itemName: %o', itemNameElement.text);
+      //price
+      let itemPrice = order.querySelector(
+        'div.container-item-col__info-item-info-additionalPrice div span'
+      );
+      let priceNumber = /^[^\d]*(\d.+)/.exec(itemPrice.innerHTML);
+      let price;
+      if (priceNumber[1]) {
+        console.log('regex price raw: ' + typeof priceNumber[1]);
+        console.log('regex price number: ' + priceNumber[1].replace(',', ''));
+        price = priceNumber[1].replace(',', '');
+      } else {
+        console.log(
+          'non regex price: ' +
+            itemPrice.innerHTML.substring(4).replace(',', '')
+        );
+        price = itemPrice.innerHTML.substring(4).replace(',', '');
+      }
+      //currency
+      let itemCurrency = /^[^(\s|\d)]*/.exec(itemPrice.innerHTML);
+      console.log('currency: %s', itemCurrency[0]);
+      let currency;
+      if (itemCurrency[0]) {
+        currency = itemCurrency[0].replace('EUR', '€').replace('US', '$');
+      }
+      //vendor
+      let vendorElement = order.querySelector(
+        'div.container-item-col__info-item-info-primary.container-item-col__info-item-info-sellerInfo div a span'
+      );
+      console.log('Haendler: %s', vendorElement.firstChild.textContent);
+      //imgUrl
+      let imgElement = order.querySelector('div.m-image img[src][alt]');
+      console.log('SRC: ' + imgElement.getAttribute('src'));
+      let imgUrl = imgElement.getAttribute('src');
+      if (imgElement.getAttribute('data-imgurl')) {
+        console.log('DATA-IMGURL: ' + imgElement.getAttribute('data-imgurl'));
+        imgUrl = imgElement.getAttribute('data-imgurl');
+      }
+      //build object for later json marshal
+      var orderObj = {
+        //   artikelnummer: parseint(artikelnummer.getattribute('data-listing-id')),
+        itemName: itemNameElement.text,
+        price: parseInt(price),
+        vendor: vendorElement.firstChild.textContent,
+        imgUrl: imgUrl,
+        purchaseDate: dateElement.textContent
+          .replace('Mai', 'May')
+          .replace('Okt', 'Oct')
+          .replace('Dez', 'Dec')
+          .replace('Mär', 'Mar'),
+        currency: currency,
+      };
+      console.log(JSON.stringify(orderObj));
+      //request to api
 
-    // postData('http://localhost:8081/order/ebay', orderObj)
-    //   .then(data => {
-    //     console.log(data); // JSON data parsed by `data.json()` call
-    //   });
+      // postData('http://localhost:8081/order/ebay', orderObj)
+      //   .then(data => {
+      //     console.log(data); // JSON data parsed by `data.json()` call
+      //   });
+    }
   }
 })();
